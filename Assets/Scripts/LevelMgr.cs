78d665da3f92ca4e42339d07a351635c73781474
robs <rguerreroayon@gmail.com>
2019-10-19 10:30:20 -0700

    vuforia coppied

---
+++ Assets/Scripts/LevelMgr.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelMgr : MonoBehaviour {
+
+
+	// Usa esto para inicializar
+	void Start () {
+		
+	}
+	
+	// La actualizacion se llama una vez por frame
+	void Update () {
+		
+	}
+
+	public void planetX(string NombreNivel){
+		SceneManager.LoadScene (NombreNivel);
+	}
+
+	public void exit(){
+		Application.Quit();
+		Debug.Log("bye ");
+	}
+
+}
